4e03e918e743097ee007ad6a6fe58aecb07048a5
Younis Said <Younissaid900@gmail.com>
2025-05-24 03:17:51 +0300

    Add files via upload

---
+++ Databases/01 Data Bases/03 DDL/SQLQuery1.sql
@@ -0,0 +1,57 @@
+--P01
+--1
+select S.St_Id [Student ID] , CONCAT_WS(' ',S.St_Fname,S.St_Lname) [Full Name] ,D.Dept_Name [Depertament Name]
+from Student S join Department D 
+on S.Dept_Id = D.Dept_Id
+--2
+select I.Ins_Name ,isnull(Salary,'0000') [Salary]
+from Instructor I
+--3
+select I.Ins_Name ,COUNT(*) [Suprvsied Students]
+from Instructor I join Student S
+on S.St_super = I.Ins_Id
+group by I.Ins_Name
+--4
+select MIN(Salary) [Min Salary], Max(Salary) [Max Salary]
+from Instructor
+--5
+select AVG(Salary) [AVG Salary]
+from Instructor
+--6
+select * 
+from Instructor
+where Salary < (select AVG(Salary) [AVG Salary]
+from Instructor)
+--7
+select D.Dept_Name 
+from Department D join Instructor I
+on D.Dept_Id = I.Dept_Id 
+where I.Salary = (select MIN(Salary) from Instructor)
+--8
+select distinct top 2 Salary
+from Instructor I
+order by Salary desc
+
+
+--P02
+--1
+select D.Dname , MAX(Salary) [Max Salary] , MIN(Salary) [Min Salary] , AVG(Salary) [AVG Salary]
+from Departments D join Employee E 
+on D.Dnum = E.Dno
+group by D.Dname
+--2
+select D.* 
+from Departments D
+join Employee E ON D.Dnum = E.Dno
+where E.SSN = (select MIN(SSN) from Employee);
+--3
+select distinct top 2 Salary
+from Employee E 
+where SSN in (
+select  E.SSN
+from Employee E
+)
+order by Salary desc
+
+
+
